631297a54ed45ccbe9e288ecab55dc02bc29054e
GreenSeed26 <christian.narte29@gmail.com>
2024-06-21 14:11:40 +0800

    3

--- app/(profile)/[nametag]/page.tsx
+++ app/(profile)/[nametag]/page.tsx
@@ -5,20 +5,6 @@ import { GetServerSideProps } from "next";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
-export async function getProfile(nametag: string) {
-  try {
-    const res = await fetch(`/api/user/${nametag}`, {
-      cache: "no-store",
-    });
-    if (res.ok) {
-      const users = await res.json();
-      return users;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 async function ProfilePage({ params }: { params: { nametag: string } }) {
   const session = await getServerSession(authOptions);
 
